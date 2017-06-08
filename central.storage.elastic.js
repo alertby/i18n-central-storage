7eed3fc55bca8230d51e79a2c811eabf759ae9d4
Andrew Rozhkov <a.rozhkov@codemaster.by>
2017-06-08 18:13:43 +0300

    stage

--- central.storage.elastic.js
+++ central.storage.elastic.js
@@ -1,4 +1,5 @@
 import es from 'elasticsearch';
+import crypto from 'crypto';
 
 export default class ElasticCentralStorage {
     constructor(config) {
@@ -7,6 +8,12 @@ export default class ElasticCentralStorage {
             throw Error('ElasticCentralStorage host should be specified');
         }
 
+        if (!config.index) {
+            throw Error('ElasticCentralStorage index should be specified');
+        }
+
+        this.config = config;
+
         this.client = new es.Client({
           host: config.host,
           log: 'trace'
@@ -14,7 +21,34 @@ export default class ElasticCentralStorage {
     }
 
     fetch(messages, locale) {
-        // @todo
+        const promise = new Promise((resolve, reject) => {
+            const docs = messages.map((message) => {
+                const hash = crypto.createHmac('sha256', this.config.index)
+                       .update(message)
+                       .digest('hex');
+
+                const doc = {
+                    _index: this.config.index,
+                    _type: locale,
+                    _id: hash
+                };
+                return doc;
+            });
+
+
+            this.client.mget({
+                body: {
+                    docs
+                }
+            }, (error, response) => {
+                if (error) {
+                    reject(error);
+                }
+                resolve(response);
+            });
+        });
+
+        return promise;
     }
 
 }
--- test/central.storage.elastic.test.js
+++ test/central.storage.elastic.test.js
@@ -1,5 +1,7 @@
 /* global __dirname, describe, it, beforeEach, afterEach */
 import ElasticCentralStorage from '../central.storage.elastic';
+import moment from 'moment';
+import crypto from 'crypto';
 import should from 'should';
 
 
@@ -8,7 +10,18 @@ describe('ElasticCentralStorage', () => {
     let elasticCentralStorage = null;
     const config = {
         host: '192.168.1.237:9200',
-        index: 'i18n-central-storage'
+        index: 'i18n-central-storage-test'
+    };
+    const mappings = {
+        'messages': {
+            '_all': {'enabled': false },
+            'properties': {
+                'message': {'type': 'text'},
+                'translattion': {'type': 'text'},
+                'translatedAt': {'type': 'date'},
+                'publishedAt': {'type': 'date'}
+            }
+        }
     };
 
     beforeEach((done) => {
@@ -16,7 +29,10 @@ describe('ElasticCentralStorage', () => {
         elasticCentralStorage = new ElasticCentralStorage(config);
 
         elasticCentralStorage.client.indices.create({
-            index: config.index
+            index: config.index,
+            body: {
+                mappings
+            }
         }, (error) => {
             if (error) {
                 throw Error(error);
@@ -28,27 +44,60 @@ describe('ElasticCentralStorage', () => {
     });
 
     afterEach((done) => {
-        elasticCentralStorage.client.indices.delete({
-            index: config.index
-        }, (error) => {
-            if (error) {
-                throw Error(error);
-            }
-            done();
+        // elasticCentralStorage.client.indices.delete({
+        //     index: config.index
+        // }, (error) => {
+        //     if (error) {
+        //         throw Error(error);
+        //     }
+        //     done();
 
+        // });
+        done();
+    });
+
+    it('add', (done) => {
+        const locale = 'ru';
+        const message = 'test message 1';
+        const secret = config.index;
+        const hash = crypto.createHmac('sha256', secret)
+                   .update(message)
+                   .digest('hex');
+
+        elasticCentralStorage.client.create({
+            index: config.index,
+            type: locale,
+            id: hash,
+            body: {
+                message,
+                translatedAt: null,
+                publishedAt: moment().format('YYYY-MM-DDTHH:mm:ss')
+            }
+        }).
+        then((error, response) => {
+            should.not.exist(error);
+            done();
         });
     });
 
 
     it('fetch', (done) => {
 
+        const messages = [
+            'test message 1',
+            'test message 2'
+        ];
+        const locale = 'ru';
+
 
         elasticCentralStorage.client.ping({
                 requestTimeout: 30000
-        }, (err) => {
-            should.not.exist(err);
+        }, (error) => {
+            should.not.exist(error);
 
-            elasticCentralStorage.fetch(messages, locale);
+            elasticCentralStorage
+                .fetch(messages, locale)
+                .then(() => { done(); });
         });
 
     });
